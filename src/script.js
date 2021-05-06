1ed818e020432b6b6c587861a79c7b9849ac77ae
Verbeshot <plun678@gmail.com>
2021-05-06 10:50:53 -0700

    v.0.0.11

--- src/script.js
+++ src/script.js
@@ -25,7 +25,7 @@ const textureLoader = new THREE.TextureLoader();
 var fontLoader = new THREE.FontLoader();
 
 // Debug
-const gui = new dat.GUI()
+// const gui = new dat.GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -60,10 +60,10 @@ material_skybox.color = new THREE.Color(0x000000);
 // ];
 
 const skybox = new THREE.Mesh(geometry_skybox,material_skybox);
-scene.add(skybox);
+// scene.add(skybox);
 skybox.material.side = BackSide;
 skybox.material.transparent = true;
-skybox.material.opacity = 0.75;
+skybox.material.opacity = 0;
 
 
 
@@ -93,10 +93,11 @@ const truck = new THREE.Object3D();
 scene.add(truck);
 
 const objLoader = new OBJLoader();
-objLoader.load('/models/Truck.obj', (root) => {
+objLoader.load('/models/Truck_0.obj', (root) => {
     truck.add(root);
-    root.scale.set(0.01,0.01,0.01);
+    root.scale.set(0.07,0.07,0.07);
     root.rotateY(Math.PI);
+    root.rotateX(-Math.PI/2);
     root.position.setY(-0.54);
     root.castShadow = true;
     root.receiveShadow = true;
@@ -149,13 +150,11 @@ function updateTrucktext(hours,minutes,seconds) {
         if (clockscale<0.2) {clocktext.children[0].material.opacity = 0.2+(clockscale);}
         if (clockscale>1) {clocktext.children[0].material.opacity = 0.4*(1/clockscale)**2;}
 
-        // if (seconds%2==0) {clocktext.position.set(0,1,0);} 
-        // else {clocktext.position.set(0,0,0)}
-
         // clocktext.scale.set(clockscale,clockscale,clockscale);
 
         // clocktext.rotation.y = Math.atan2( ( camera.position.x - clocktext.position.x ), ( camera.position.z - clocktext.position.z ) );
     } );
+
 }
 
 fontLoader.load( '/fonts/helvetiker_regular.typeface.json', function ( font ) {
@@ -225,21 +224,6 @@ road.receiveShadow = true;
 // road.position.set(0,(planetDisplacement),0);
 
 
-/////////////////////////////////////////Planet Group/////////////////////////////////////////////
-
-const SMGroup = new THREE.Group();
-
-// Simulated Motion
-
-SMGroup.add(floor);
-SMGroup.add(road);
-// SMGroup.add(marker);
-SMGroup.position.set(0,-planetRadius+planetDisplacement,0);
-// SMGroup.add(skybox);
-// scene.add(SMGroup);
-
-// scene.add(floor);
-
 /////////////////////////////////////////Checkpoints/////////////////////////////////////////////
 
 const geometry_checkpoint = new THREE.TorusGeometry(100,10,16,200);
@@ -248,8 +232,6 @@ material_checkpoint.side = DoubleSide;
 const checkpoint = new THREE.Mesh(geometry_checkpoint,material_checkpoint);
 checkpoint.rotateY(Math.PI/2);
 checkpoint.position.set(0,planetRadius+planetDisplacement,0);
-
-SMGroup.add(checkpoint);
 
 // var angle_checkpoint = 0;
 
@@ -266,11 +248,29 @@ SMGroup.add(checkpoint);
 
 // scene.add(container_checkpoint);
 
+/////////////////////////////////////////Local Groups/////////////////////////////////////////////
+
+// Simulated Motion Group
+const SMGroup = new THREE.Group();
+
+SMGroup.add(floor);
+SMGroup.add(road);
+SMGroup.add(checkpoint);
+// SMGroup.add(marker);
+SMGroup.position.set(0,-planetRadius+planetDisplacement,0);
+
+const planetGroup = new THREE.Group();
+planetGroup.add(SMGroup);
+planetGroup.add(truck);
+planetGroup.add(clocktext);
+planetGroup.add(checkpoint);
+
 
 /////////////////////////////////////////Sun/////////////////////////////////////////////
 
 var sunRadius = planetRadius*5;
 orbitRadius = 100000;
+var orbitArcLength = 2*Math.PI;
 var y_sun = 0;
 var x_sun = 0;
 var z_sun = 0;
@@ -280,12 +280,16 @@ const material_sun = new THREE.MeshStandardMaterial();
 material_sun.roughness = 0.85;
 material_sun.metalness = 0.02;
 material_sun.color = new THREE.Color(0xF3BB80);
-material_sun.emissive = new THREE.Color(0xF3BB80);
+material_sun.emissive = new THREE.Color(0xFFFCCC);
 material_sun.emissiveIntensity = 5;
 
 const sun = new THREE.Mesh(geometry_sun,material_sun);
-scene.add(sun);
 sun.scale.set(sunRadius/10,sunRadius/10,sunRadius/10);
+
+const marker = sun.clone();
+marker.scale.set(sunRadius/50,sunRadius/50,sunRadius/50);
+marker.position.set(0,orbitRadius,0);
+
 
 const sunLight = new THREE.PointLight(0xfdfbd3, 1)
 sunLight.castShadow = true;
@@ -294,21 +298,35 @@ scene.add(sunLight);
 sunLight.shadow.camera.near = 0.3;
 
 const geometry_orbit = new THREE.TorusBufferGeometry(orbitRadius,20,16,500);
-
 const orbit = new THREE.Mesh(geometry_orbit,material_sun);
-scene.add(orbit);
 
 const sunGroup = new THREE.Group();
 sunGroup.add(sun);
 sunGroup.add(sunLight);
 sunGroup.add(orbit);
+sunGroup.add(marker);
 sunGroup.position.set(x_sun,y_sun,z_sun);
 // scene.add(sunGroup);
 
-function sunMovement(orbitLocation) {
-    x_sun = (orbitRadius-planetRadius)*Math.cos(orbitLocation);
-    y_sun = (orbitRadius-planetRadius)*Math.sin(orbitLocation);
+function setOrbitArc(orbitLocation) {
+    orbitArcLength = orbitLocation;
+    const geometry_orbitC = new THREE.TorusBufferGeometry(orbitRadius,250,16,500,orbitLocation);
+    const orbitC = new THREE.Mesh(geometry_orbitC,material_sun);
+    orbitC.rotation.z = Math.PI/2;
+    sunGroup.add(orbitC);
+}
+
+function sunMovement(orbitLocation,hours) {
+    x_sun = (orbitRadius)*Math.cos(orbitLocation+3*Math.PI/2);
+    y_sun = (orbitRadius)*Math.sin(orbitLocation+3*Math.PI/2);
     sunGroup.position.set(x_sun,y_sun,z_sun);
+
+    // if (hours == 0) {
+    //     orbitArcLength = orbitLocation;
+    //     const geometry_orbitC = THREE.TorusBufferGeometry(orbitRadius,500,16,500,orbitArcLength);
+    //     const orbitC = THREE.Mesh(geometry_orbitC,material_sun);
+    //     sunGroup.add(orbitC);
+    // }
     // console.log(orbitLocation);
 }
 
@@ -324,29 +342,22 @@ const geometry_moon = new THREE.SphereBufferGeometry(10,100,100);
 const material_moon = new THREE.MeshStandardMaterial();
 material_moon.roughness = 0.9;
 material_moon.metalness = 0.02;
-material_moon.color = new THREE.Color(0xF3BB80);
+material_moon.color = new THREE.Color(0xFFFFFF);
 material_moon.emissive = new THREE.Color(0xF3BB80);
-material_moon.emissiveIntensity = 1;
+material_moon.emissiveIntensity = 0;
 
 const moon = new THREE.Mesh(geometry_moon,material_moon);
 
 moon.scale.set(moonRadius/10,moonRadius/10,moonRadius/10);
 
-const moonLight = new THREE.PointLight(0xfdfbd3, 0.1);
-moonLight.castShadow = true;
-scene.add(moonLight);
-
-moonLight.shadow.camera.near = 0.3;
-
 const moonGroup = new THREE.Group();
 moonGroup.add(moon);
-moonGroup.add(moonLight);
 moonGroup.position.set(x_moon,y_moon,z_moon);
 scene.add(moonGroup);
 
 function moonMovement(orbitLocation) {
-    x_moon = (orbitRadius_moon-planetRadius)*Math.cos(orbitLocation);
-    y_moon = (orbitRadius_moon-planetRadius)*Math.sin(orbitLocation);
+    x_moon = (orbitRadius_moon)*Math.cos(orbitLocation);
+    y_moon = (orbitRadius_moon)*Math.sin(orbitLocation);
     moonGroup.position.set(x_moon,y_moon,z_moon);
     // console.log(orbitLocation);
 }
@@ -365,7 +376,7 @@ material_atmos.color = new THREE.Color(0xF3BB80);
 
 const atmosphere = new THREE.Mesh(geometry_atmos,material_atmos);
 // scene.add(atmosphere);
-atmosphere.position.set(0,-planetRadius+planetDisplacement,0);
+// atmosphere.position.set(0,-planetRadius+planetDisplacement,0);
 atmosphere.material.side = DoubleSide;
 atmosphere.material.transparent = true;
 
@@ -375,49 +386,54 @@ function createAtmosphere(hours,minutes) {
 
     scene.fog = new THREE.FogExp2(fogColor,atmosDensity);
 
-        if (camera.position.distanceTo(SMGroup.position) < atmosRadius1) {
-            spinObject.add(SMGroup);
-            scene.add(clocktext);
-            spinObject.remove(sunGroup);
-            atmosphere.material.opacity = -1*Math.cbrt(Math.cos(2*Math.PI*(hours+minutes/60)/24));
-            atmosDensity = -0.005*Math.cbrt(Math.cos(2*Math.PI*(hours+minutes/60)/24))+0.005;
-            scene.fog = new THREE.FogExp2(fogColor,atmosDensity);
-            controls.maxDistance = 3000;
-            controls.enablePan = false;
-            fovChange();
-            // scene.remove(atmosphere);
-        }
+    // if (camera.position.distanceTo(SMGroup.position) < planetRadius-1) {
+    //     atmosDensity = 1;
+    //     scene.fog = new THREE.FogExp2(fogColor,atmosDensity);
+    // }
 
-        if (camera.position.distanceTo(SMGroup.position) > atmosRadius1) {
-            atmosDensity = (0.001*(atmosRadius-camera.position.distanceTo(SMGroup.position)))**4;
-            scene.fog = new THREE.FogExp2(fogColor,atmosDensity);
-            atmosphere.material.opacity = 1;
-            // atmosphere.material.opacity = 1/(atmosDensity*1000;
-            // scene.add(atmosphere);
-            spinObject.add(sunGroup)
-            spinObject.remove(SMGroup);
-            scene.remove(clocktext);
+    if (camera.position.distanceTo(SMGroup.position) < atmosRadius1) {
+        scene.add(planetGroup);
+        spinObject.remove(sunGroup);
+        atmosphere.material.opacity = -1*Math.cbrt(Math.cos(2*Math.PI*(hours+minutes/60)/24));
+        atmosDensity = -0.005*Math.cbrt(Math.cos(2*Math.PI*(hours+minutes/60)/24))+0.005;
+        scene.fog = new THREE.FogExp2(fogColor,atmosDensity);
+        controls.maxDistance = 600;
+        controls.enablePan = false;
+        fovChange();
+        // scene.remove(atmosphere);
+    }
 
-            controls.maxDistance = 200000;
-            controls.enablePan = true;
+    if (camera.position.distanceTo(SMGroup.position) > atmosRadius1) {
+        atmosDensity = (0.001*(atmosRadius-camera.position.distanceTo(SMGroup.position)))**4;
+        scene.fog = new THREE.FogExp2(fogColor,atmosDensity);
+        atmosphere.material.opacity = 1;
+        // atmosphere.material.opacity = 1/(atmosDensity*1000;
+        // scene.add(atmosphere);
+        spinObject.add(sunGroup);
+        scene.remove(planetGroup);
 
-            camera.fov = 60;
+        skybox.material.opacity = 0.75;
 
-            hud.innerHTML = "Click To Go Back";
-            hud.classList.add('back');
+        controls.maxDistance = 200000;
+        controls.enablePan = true;
 
-            hud.addEventListener("click", () => {camera.position.set(0,10,104); controls.reset()});
-        }
+        camera.fov = 60;
 
-        if (camera.position.distanceTo(SMGroup.position) < atmosRadius) {
-            spinObject.add(skybox);
-        }
+        hud.innerHTML = "Click To Go Back";
+        hud.classList.add('back');
 
-        if (camera.position.distanceTo(SMGroup.position) > atmosRadius) {
-            atmosDensity = 0;
-            scene.fog = new THREE.FogExp2(fogColor,atmosDensity);
-            spinObject.remove(skybox);
-        }
+        hud.addEventListener("click", () => {camera.position.set(0,10,104); controls.reset()});
+    }
+
+    if (camera.position.distanceTo(SMGroup.position) < atmosRadius) {
+        scene.add(skybox);
+    }
+
+    if (camera.position.distanceTo(SMGroup.position) > atmosRadius) {
+        atmosDensity = 0;
+        scene.fog = new THREE.FogExp2(fogColor,atmosDensity);
+        scene.remove(skybox);
+    }
 
     // console.log(atmosDensity);
 }
@@ -428,10 +444,8 @@ function createAtmosphere(hours,minutes) {
 const spinObject = new THREE.Group();
 spinObject.add(atmosphere);
 spinObject.add(sunGroup);
-spinObject.add(SMGroup);
-// spinObject.add(truck);
-// spinObject.add(clocktext);
-spinObject.add(skybox);
+spinObject.add(moonGroup);
+spinObject.position.set(0,-planetRadius+planetDisplacement,0);
 scene.add(spinObject);
 
 
@@ -449,19 +463,19 @@ const pLightColor = {
     color: 0xfdfbd3
 }
 
-const pLightDebug = gui.addFolder("pLight")
+// const pLightDebug = gui.addFolder("pLight")
 
 // const pLightHelper = new THREE.PointLightHelper(pLight,1)
 // scene.add(pLightHelper)
 
-pLightDebug.add(pLight.position, "x").min(-5).max(5).step(0.01)
-pLightDebug.add(pLight.position, "y").min(-5).max(20).step(0.01)
-pLightDebug.add(pLight.position, "z").min(-5).max(5).step(0.01)
-pLightDebug.add(pLight, "intensity").min(0).max(10).step(0.01)
-pLightDebug.addColor(pLightColor, "color")
-    .onChange(() => {
-        pLight.color.set(pLightColor.color)
-    })
+// pLightDebug.add(pLight.position, "x").min(-5).max(5).step(0.01)
+// pLightDebug.add(pLight.position, "y").min(-5).max(20).step(0.01)
+// pLightDebug.add(pLight.position, "z").min(-5).max(5).step(0.01)
+// pLightDebug.add(pLight, "intensity").min(0).max(10).step(0.01)
+// pLightDebug.addColor(pLightColor, "color")
+//     .onChange(() => {
+//         pLight.color.set(pLightColor.color)
+//     })
 
 // const hemLight = new THREE.HemisphereLight(0xEB4678, 0x8CEBE7, 0.5 );
 // hemLight.position.set(-2,3,-4);
@@ -497,7 +511,7 @@ window.addEventListener('resize', () =>
 
 /////////////////////////////////////////Camera/////////////////////////////////////////////
 // Base camera
-const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 300000);
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 400000);
 camera.position.x = 0;
 camera.position.y = 10;
 camera.position.z = 104;
@@ -532,6 +546,8 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 const clock = new THREE.Clock()
 const init_time = new Date();
 const init_seconds = init_time.getSeconds();
+const init_year = init_time.getFullYear();
+const init_timeTotal = (Math.floor(init_time.getTime()/1000)-(init_year-1970)*31557600)/31557600;
 
 const tick = () => {
 
@@ -543,7 +559,7 @@ const tick = () => {
     var seconds = time.getSeconds();
     var mseconds = time.getMilliseconds();
 
-    var timeTotal = 2*Math.PI*(Math.floor(time.getTime()/1000))/31557600;
+    var timeTotal = 2*Math.PI*(Math.floor(time.getTime()/1000)-(init_year-1970)*31557600)/31557600;
 
     // Update objects
     box.rotation.y = -2010*elapsedTime/60;
@@ -559,13 +575,13 @@ const tick = () => {
 
     // updateCheckpoint(init_seconds,elapsedTime);
 
-    sunMovement(timeTotal);
+    sunMovement(timeTotal,hours);
 
     moonMovement(timeTotal*31557600/2358720);
 
     // planetSpin(minutes,hours);
 
-    spinObject.rotation.z = 2*Math.PI*(hours*60+minutes)/1440+Math.PI;
+    spinObject.rotation.z = -2*Math.PI*(hours*60+minutes)/1440-Math.PI/2;
 
     // console.log(camera.position);
 
@@ -583,6 +599,11 @@ const tick = () => {
 }
 
 tick();
+setOrbitArc(2*Math.PI*init_timeTotal);
+
 console.log(atmosDensity);
 console.log(atmosphere.material.opacity);
 console.log(camera.fov);
+console.log(spinObject.position);
+console.log(atmosphere.position);
+console.log(init_timeTotal);
